14c8fb3de23c986f80f3d8172581eb7fc3539b83
HideyoshiHitoro <90239094+HideyoshiHitoro@users.noreply.github.com>
2022-04-25 22:14:59 +0700

    hmm

--- plugins/image-pinterest.js
+++ plugins/image-pinterest.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
   if (!text) throw `Example use ${usedPrefix + command} minecraft`
   const json = await pinterest(text)
-  await conn.sendButtonImg(m.chat, 'Pinterest', watermark, await(await fetch(json.getRandom())).buffer(),[['Cari lagi', `.pinterest ${text}`]], m)
+  await conn.sendButton(m.chat, 'Pinterest', watermark, await(await fetch(json.getRandom())).buffer(),[['Cari lagi', `.pinterest ${text}`]], m)
 }
 handler.help = ['pinterest <keyword>']
 handler.tags = ['internet']
